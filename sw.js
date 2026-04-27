f2a7010e9cc675494eb1b28621556a64501b84a7
waterbusiness961-hue <waterbusiness961@gmail.com>
2026-04-27 15:31:18 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,9 +6,7 @@ const urlsToCache = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
@@ -19,8 +17,6 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
